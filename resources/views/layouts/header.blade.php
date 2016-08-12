e4a9e006bb1cc7c10754f71aa008d347760a252d
tianjiawei <tianjiawei@ubox.cn>
2016-08-12 16:34:30 +0800

    common header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>田佳伟的博客</title>
+
+    <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="title" style="text-align: center;">
+    <h1>田佳伟的博客</h1>
+    <div style="padding: 5px; font-size: 16px;">开发中，敬请期待……</div>
+</div>
+<hr>
